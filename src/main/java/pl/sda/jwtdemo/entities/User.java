2cad4cbae251422ea7731cd7b45b87d07baab85e
Marcin Szupke <marcin.szupke@gmail.com>
2023-12-17 10:56:09 +0100

    add lombok, add user entity

---
+++ src/main/java/pl/sda/jwtdemo/entities/User.java
@@ -0,0 +1,36 @@
+package pl.sda.jwtdemo.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+}
