6fd0def5ca094cdbc2ee771436a7b47451c70b8c
Marcin Szupke <marcin.szupke@gmail.com>
2023-12-17 12:40:26 +0100

    store jwt values in application.properties

--- src/main/java/pl/sda/jwtdemo/services/JwtService.java
+++ src/main/java/pl/sda/jwtdemo/services/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +18,10 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
+    @Value("${security.jwt.expiration-time}")
     private long jwtExpiration;
 
+    @Value("${security.jwt.secret-key}")
     private String secretKey;
 
     public String generateToken(UserDetails userDetails) {
